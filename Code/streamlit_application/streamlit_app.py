ea1cc7b9cfbf8a1cc4a8db6efa710cc3a2bbc63e
Aditya <rajurajalala@gmail.com>
2023-12-19 18:05:40 +0000

    almost completed

--- Code/streamlit_application/streamlit_app.py
+++ Code/streamlit_application/streamlit_app.py
@@ -1,10 +1,13 @@
+
 import streamlit as st
 from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_code_pb2
 from PIL import Image
+import textwrap
+import google.generativeai as genai
+from IPython.display import Markdown
 
-# Clarifai API code
 def predict_food_item(image_bytes, pat, user_id, app_id, model_id, model_version_id):
     channel = ClarifaiChannel.get_grpc_channel()
     stub = service_pb2_grpc.V2Stub(channel)
@@ -39,7 +42,39 @@ def predict_food_item(image_bytes, pat, user_id, app_id, model_id, model_version
     return output
 
 # Streamlit application code
-st.title("Food Recognition App")
+
+
+st.title("Gastronomix AI: Culinary Insight Hub")
+
+
+
+st.markdown(
+    """
+    <span style="font-size: 18px;"> Gastronomix AI stands as an innovative food inventory management solution, 
+    incorporating cutting-edge technology to minimize food waste and enhance user convenience. Bid farewell to 
+    uncertainties when managing leftovers; Culinary Guardian steps in as your culinary ally. Utilize the phone's camera 
+    or manual text entry to seamlessly identify food products. With its state-of-the-art object recognition, Culinary 
+    Guardian redefines culinary management, ensuring a smooth and efficient experience in organizing your kitchen inventory.</span>
+    """,
+    unsafe_allow_html=True
+)
+
+# page_bg_img = f"""
+# <style>
+# [data-testid="stAppViewContainer"] > .main {{
+# background-image: url("https://i.postimg.cc/Df66JRLg/logo-black.jpg");
+# background-size: cover;
+# background-position: center center;
+# background-repeat: no-repeat;
+# background-attachment: local;
+# }}
+# [data-testid="stHeader"] {{
+# background: rgba(0,0,0,0);
+# }}
+# </style>
+# """
+
+# st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Sidebar option to choose between camera or upload
 option = st.sidebar.radio("Choose an option", ["Capture from Camera", "Upload Image"])
@@ -47,11 +82,26 @@ option = st.sidebar.radio("Choose an option", ["Capture from Camera", "Upload Im
 # Function to capture image from camera
 def capture_from_camera():
     st.markdown("### Capture Image")
-    captured_image = st.camera_input("Capture Image")
-    return captured_image
+    capture_button = st.button("Capture Image")
+    
+    if capture_button:
+        captured_image = st.camera_input("Captured Image")
+        return captured_image
+    else:
+        return None
+    # st.markdown("### Capture Image")
+    # captured_image = st.camera_input("Capture Image")
+    # return captured_image
 
 # Function to upload image
 def upload_image():
+    # upload_button = st.button("Upload Image")
+    
+    # if upload_button:
+    #     captured_image = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
+    #     return uploaded_file
+    # else:
+    #     return None
     st.markdown("### Upload Image")
     uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
     return uploaded_file
@@ -74,6 +124,9 @@ if st.button("Recognize Food"):
         image_bytes = uploaded_file.read()
         # Display the uploaded image at its standard size
         st.image(Image.open(uploaded_file), caption="Uploaded Image", use_column_width=True)
+    else :
+        st.markdown("### Please capture or upload an image")
+        st.stop()
 
     # Get Clarifai predictions using the defined function
     st.markdown("### Predicted Concepts:")
@@ -90,3 +143,32 @@ if st.button("Recognize Food"):
     # Display predicted concepts horizontally
     concepts = [f"{concept.name} {concept.value:.2f}" for concept in predicted_output.data.concepts]
     st.write(", ".join(concepts))
+
+    # Display a separate section for concepts with value above or equal to 0.85
+    st.markdown("### High Confidence Concepts:")
+    high_confidence_concepts = [f"{concept.name} {concept.value:.2f}" for concept in predicted_output.data.concepts if concept.value >= 0.85]
+    st.write(", ".join(high_confidence_concepts))
+
+# New section for generating recipe suggestions
+st.sidebar.markdown("## Recipe Suggestions")
+
+# Get user input for the ingredient
+ingredient_prompt = st.sidebar.text_input("Enter ingredients to recieve recipe:", "")
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+GOOGLE_API_KEY='AIzaSyDJ4815fNqfkwvpp4m6qCXjmU9V8QoTaWw'
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# Create a button to trigger recipe generation
+if st.sidebar.button("Generate Recipes"):
+    # Use the generative AI model to generate content
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(f"what recipes can I make with {ingredient_prompt}")
+
+    # Display the generated content
+    st.sidebar.markdown("### Recipe Suggestions:")
+    st.sidebar.markdown(response.text)
+
